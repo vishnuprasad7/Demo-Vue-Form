d4b96554603bada17bd4fe705a009b601218c5b7
Vishnu Prasad <vpsvprasad8@gmail.com>
2019-05-13 14:44:07 +0530

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<template lang="pug">
+  #app.section
+    form-json(:formFields="jsonFields",
+              :formName="'userProfil'")
+</template>
+
+<script>
+import 'bulma/css/bulma.min.css'
+import 'vue-form-json/dist/vue-form-json.css'
+import formJson from 'vue-form-json'
+import jsonFields from '@/assets/fields'
+
+export default {
+  name: 'app',
+  components: {
+    formJson
+  },
+  data: () => ({
+    jsonFields
+  }),
+  mounted () {
+    this.$root.$on('formSubmitted', values => alert(JSON.stringify(values)))
+  }
+}
+</script>
---
+++ src/main.js
@@ -0,0 +1,16 @@
+// The Vue build version to load with the `import` command
+// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import Vue from "vue";
+import VeeValidate from "vee-validate";
+import App from "./App";
+
+Vue.use(VeeValidate);
+
+Vue.config.productionTip = false;
+
+/* eslint-disable no-new */
+new Vue({
+  el: "#app",
+  components: { App },
+  template: "<App/>"
+});
