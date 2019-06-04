fa1345308112cb5dfda223b02365b6b0cacbdb9a
Vishnu Prasad <vpsvprasad8@gmail.com>
2019-06-04 16:08:33 +0530

    ui change
    responsive table layout

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,80 @@
 
 <style>
-#table-wrapper {
-  display: table;
-}
-#desc {
-  /* width: 15em; */
-  border-collapse: collapse;
-  border-radius: 50px;
-}
-#desc th {
-  background-color: whitesmoke;
-  padding: 1em;
-  text-align: center;
-  border: 1px solid black;
-  padding: 5px;
-  width: 100%;
-}
-
-#desc td {
-  border: 1px solid black;
-  padding: 5px;
-  text-align: center;
+@media screen and (max-width: 800px) {
+  .is-responsive {
+    width: 100%;
+    border-collapse: collapse;
+    border-spacing: 0;
+    display: block;
+    position: relative;
+  }
+  .is-responsive td:empty:before {
+    content: " ";
+  }
+  .is-responsive th,
+  .is-responsive td {
+    margin: 0;
+    vertical-align: top;
+  }
+  .is-responsive th {
+    text-align: left;
+  }
+  .is-responsive thead {
+    border-right: solid 2px #dbdbdb;
+    display: block;
+    float: left;
+  }
+  .is-responsive thead tr {
+    display: block;
+    padding: 0 10px 0 0;
+  }
+  .is-responsive thead tr th::before {
+    content: " ";
+  }
+  .is-responsive thead td,
+  .is-responsive thead th {
+    border-width: 0 0 1px;
+  }
+  .is-responsive tbody {
+    display: block;
+    width: auto;
+    position: relative;
+    overflow-x: auto;
+    white-space: nowrap;
+  }
+  .is-responsive tbody tr {
+    display: inline-block;
+    vertical-align: top;
+  }
+  .is-responsive th {
+    display: block;
+    text-align: right;
+  }
+  .is-responsive td {
+    display: block;
+    min-height: 1.25em;
+    text-align: left;
+  }
+  .is-responsive th:last-child,
+  .is-responsive td:last-child {
+    border-bottom-width: 0;
+  }
+  .is-responsive tr:last-child td:not(:last-child) {
+    border: 1px solid #dbdbdb;
+    border-width: 0 0 1px;
+  }
+  .is-responsive.is-bordered td,
+  .is-responsive.is-bordered th {
+    border-width: 1px;
+  }
+  .is-responsive.is-bordered tr td:last-child,
+  .is-responsive.is-bordered tr th:last-child {
+    border-bottom-width: 1px;
+  }
+  .is-responsive.is-bordered tr:last-child td,
+  .is-responsive.is-bordered tr:last-child th {
+    border-width: 1px;
+  }
 }
 </style>
 
@@ -29,19 +83,17 @@
   #app.section
     form-json(:formFields="jsonFields",
               :formName="'userProfile'")
-
-    #table-wrapper
-      table(class="table is-responsive" v-if="resultantObj.length !== 0")
+    table(class='table is-responsive' v-if='resultantObj.length != 0')
+      thead
         tr
-          th(colspan=7) User Information
-        tr
-          td UID
-          td Source
-          td Destination
-          td Direction
-          td Sent Time
-          td Total Cost
-          td Status
+          th UID
+          th Source
+          th Destination
+          th Direction
+          th Sent Time
+          th Total Cost
+          th Status
+      tbody
         tr(v-for="item in resultantObj")
           td {{item.uid}}
           td {{item.source}}
@@ -69,41 +121,37 @@ export default {
   },
   data: () => ({
     jsonFields,
-    resultantObj: [{}]
+    resultantObj: ""
   }),
 
   mounted() {
-    this.$root.$on("formSubmitted", values =>
-      // console.log(JSON.stringify(values)
-      {
-        axios({
-          method: "get",
-          url: "https://api.karix.co/message/",
-          auth: {
-            username: "6e9d22a6-2e74-439e-888b-721fa1312251",
-            password: "e8e7f546-b60d-4efd-9717-9766f4f087b9"
-          }
-        }).then(response => {
-          // console.log(response.data.objects);
-          var responseObj = response.data.objects;
-          var res = JSON.stringify(responseObj);
-          const result = JSON.parse(res).map(item => {
-            return {
-              uid: item.account_uid,
-              source: item.source,
-              destination: item.destination,
-              direction: item.direction,
-              sentTime: item.sent_time,
-              totalCost: item.total_cost,
-              status: item.status
-            };
-          });
-          // var temp = JSON.stringify(result);
-          // console.log(temp);
-          this.resultantObj = result;
+    this.$root.$on("formSubmitted", values => {
+      axios({
+        method: "get",
+        url: "https://api.karix.co/message/",
+        auth: {
+          username: "6e9d22a6-2e74-439e-888b-721fa1312251",
+          password: "e8e7f546-b60d-4efd-9717-9766f4f087b9"
+        }
+      }).then(response => {
+        // console.log(response.data.objects);
+        var responseObj = response.data.objects;
+        var res = JSON.stringify(responseObj);
+        const result = JSON.parse(res).map(item => {
+          return {
+            uid: item.account_uid,
+            source: item.source,
+            destination: item.destination,
+            direction: item.direction,
+            sentTime: item.sent_time,
+            totalCost: item.total_cost,
+            status: item.status
+          };
         });
-      }
-    );
+        this.resultantObj = result;
+      });
+    });
+
     //http get request using axios
   }
 };
